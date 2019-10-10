45dcbfa47a2c761f61025d9713679d74fe2d10e6
Gina Sprint <sprint@gonzaga.edu>
2019-10-10 14:38:48 -0700

    jpanel setup

--- src/DialogTester.java
+++ src/DialogTester.java
@@ -3,21 +3,23 @@ import java.util.Scanner;
 
 public class DialogTester {
     public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
-        int userNum = 0;
+//        Scanner kb = new Scanner(System.in);
+//        int userNum = 0;
+//
+//        boolean valid = true;
+//        do {
+//            valid = true;
+//            System.out.print("Enter an integer (or else!): ");
+//            try {
+//                userNum = kb.nextInt();
+//            } catch (InputMismatchException e) {
+//                System.out.println("Hey, that's not an integer!!");
+//                valid = false;
+//            } finally {
+//                kb.nextLine(); // flush the input buffer
+//            }
+//        } while(!valid);
 
-        boolean valid = true;
-        do {
-            valid = true;
-            System.out.print("Enter an integer (or else!): ");
-            try {
-                userNum = kb.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Hey, that's not an integer!!");
-                valid = false;
-            } finally {
-                kb.nextLine(); // flush the input buffer
-            }
-        } while(!valid);
+        new SquareInputView();
     }
 }
--- src/SquareInputView.java
+++ src/SquareInputView.java
@@ -1,5 +1,52 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SquareInputView extends JFrame {
+
+    public SquareInputView() {
+        super("Squarer");
+
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(200, 200));
+
+        setupUI();
+        pack();
+    }
+
+    private void setupUI() {
+        // JLabel, then JTextField, then JButton, then JLabel
+        JPanel contentPanel = new JPanel();
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+
+        JLabel promptLabel = new JLabel("Enter an integer (or else): ");
+        contentPanel.add(promptLabel);
+
+        JTextField inputTextField = new JTextField();
+        contentPanel.add(inputTextField);
+
+        JLabel statusLabel = new JLabel(" ");
+
+        JButton calculateButton = new JButton("Calculate!");
+        calculateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text = inputTextField.getText();
+                try {
+                    int value = Integer.parseInt(text);
+                    statusLabel.setText("Squared: " + value * value);
+                } catch(NumberFormatException ex) {
+                    statusLabel.setText("Hey that's not an integer");
+                }
+            }
+        });
+        contentPanel.add(calculateButton);
+        contentPanel.add(statusLabel);
+
+
+        getContentPane().add(contentPanel);
+    }
 
 }
